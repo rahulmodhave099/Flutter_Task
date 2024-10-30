64c837f60bfd050d9b0c0b5c9455561437dfb616
rahulmodhave099 <rahulmodhave099@gmail.com>
2024-10-30 17:59:48 +0530

    sqflite connectivity with zomato app demo

--- Class_App/sqflite/zomato_sqflite/lib/main.dart
+++ Class_App/sqflite/zomato_sqflite/lib/main.dart
@@ -3,23 +3,70 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'zomato_model.dart';
 
+dynamic database;
+
+void insertData(Zomato zObj)async {
+  Database localDB = await database;
+
+  await localDB.insert("Zomato", zObj.zomatoMap(),conflictAlgorithm : ConflictAlgorithm.replace);
+}
+
+Future<List<Map<String,dynamic>>> showData()async {
+  Database localDB = await database;
+
+  List<Map<String,dynamic>> data = await localDB.query("Zomato");
+
+  return data;
+}
+
+void updateData(Zomato zObj)async {
+  Database localDB = await database;
+
+  await localDB.update("Zomato", zObj.zomatoMap(),where: "orderId=?",whereArgs: [zObj.orderId]);
+}
+
+void deleteData(Zomato zObj)async{
+  Database localDB = await database;
+
+  await localDB.delete("Zomato",where: "orderId=?",whereArgs: [zObj.orderId]);
+}
+
 void main()async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  openDatabase(
+  database = openDatabase(
     join(await getDatabasesPath(),"ZomatoDB.db"),
     version: 1,
-    onCreate: (db,version){
-      db.query(
+    onCreate: (db,version)async{
+      await db.execute(
         '''CREATE TABLE Zomato(
             orderId INT PRIMARY KEY,
             custName TEXT,
             hotelName TEXT,
             food TEXT,
-            bill REAl
+            bill REAL
         )''');
     },
   );
+
+  Zomato order1 = Zomato(orderId: 201, custName: "Sanket", hotelName: "McDonalds", food: "Maharaja Veg Burger,Peri Peri Cheesy Fries,Coke", bill: 469);
+
+  Zomato order2 = Zomato(orderId: 202, custName: "Rahul", hotelName: "McDonalds", food: "Maharaja Nonveg Burger,Fries,Coke,Nudgets", bill: 569);
+
+  insertData(order1);
+  insertData(order2);
+
+  print(await showData());
+
+  order2 = Zomato(orderId: order2.orderId, custName: order2.custName, hotelName: order2.hotelName, food: "Maharaja Nonveg Burger,Coke,Nudgets", bill: 387);
+
+  updateData(order2);
+
+  print(await showData());
+
+  deleteData(order1);
+
+  print(await showData());
 
 }
--- Class_App/sqflite/zomato_sqflite/lib/zomato_model.dart
+++ Class_App/sqflite/zomato_sqflite/lib/zomato_model.dart
@@ -1,1 +1,19 @@
-// TODO Implement this library.
+class Zomato{
+  final int orderId;
+  final String custName;
+  final String hotelName;
+  final String food;
+  final double bill;
+
+  Zomato({
+    required this.orderId,
+    required this.custName,
+    required this.hotelName,
+    required this.food,
+    required this.bill,
+  });
+
+  Map<String,dynamic> zomatoMap(){
+    return {"orderId":orderId,"custName":custName,"hotelName":hotelName,"food":food,"bill":bill};
+  }
+}
