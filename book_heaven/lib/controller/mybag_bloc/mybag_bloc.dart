a13db13866a8fa44e4e8a756c02e7b172bcff25f
rahulmodhave099 <rahulmodhave099@gmail.com>
2025-02-21 16:00:57 +0530

    Task Submission

--- book_heaven/lib/controller/mybag_bloc/mybag_bloc.dart
+++ book_heaven/lib/controller/mybag_bloc/mybag_bloc.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
 import 'package:book_heaven/controller/mybag_bloc/mybag_event.dart';
 import 'package:book_heaven/controller/mybag_bloc/mybag_state.dart';
@@ -15,19 +13,14 @@ class MyBagBloc extends Bloc<MybagEvent, MyBagState> {
   }
   FutureOr<void> onMyBagInitialEvent(
       OnMyBagInitialEvent event, Emitter<MyBagState> emit) async {
-    log('Emit CartLoadingState');
 
     emit(MyBagLoadingState());
-    log('pause');
-
-    log('Emit CartLoadedSuccessState');
 
     emit(MyBagDataState(cartList: cartList));
   }
 
   FutureOr<void> onMyBagRemoveBookEvent(
       OnMyBagRemoveBookEvent event, Emitter<MyBagState> emit) async {
-    log('Remove Product from cart');
     cartList.remove(event.book);
 
     ///To show List after removing items from cart
@@ -39,9 +32,7 @@ class MyBagBloc extends Bloc<MybagEvent, MyBagState> {
 
   FutureOr<void> onIncreBookCountEvent(
       OnIncreBookCountEvent event, Emitter<MyBagState> emit) {
-    log("Incrementing product count for ${event.book.bookName}");
 
-    // Update the product's `numberOfItems`
     event.book.quantity += 1;
 
     // Emit a state to refresh the cart view
@@ -50,16 +41,13 @@ class MyBagBloc extends Bloc<MybagEvent, MyBagState> {
 
   FutureOr<void> onDecreBookCountEvent(
       OnDecreBookCountEvent event, Emitter<MyBagState> emit) {
-    log("Decrementing product count for ${event.book.bookName}");
 
     if (event.book.quantity > 1) {
-      // Update the product's `numberOfItems`
       event.book.quantity -= 1;
 
       // Emit a state to refresh the cart view
       emit(MyBagDataState(cartList: cartList));
     } else {
-      log("Cannot decrement below 1");
     }
   }
 }
